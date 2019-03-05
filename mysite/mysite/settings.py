be6ee40c9301a7caf7bd64b2b473588f4c12783f
hejp009 <hejunpeng2012@hotmail.com>
2019-03-05 11:02:48 +0800

    add forms

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'taggit',
+
     'blog.apps.BlogConfig',
 ]
 
@@ -108,3 +110,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
